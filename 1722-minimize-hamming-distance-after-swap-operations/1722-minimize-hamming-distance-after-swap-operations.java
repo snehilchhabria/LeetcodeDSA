c845366f4def9095cd64df54152f8173110a8985
Snehil Chhabria <114361719+snehilchhabria@users.noreply.github.com>
2023-07-04 17:47:01 +0530

    Time: 52 ms (92.05%), Space: 85.8 MB (32.95%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        
+        int n = source.length, answer = 0;
+        //union the cells that can be connected thru swaps
+        UnionFind uf = new UnionFind(n);
+        for (int[] swap : allowedSwaps) {
+            uf.unify(swap[0], swap[1]);
+        }
+        //this holds the element in the source array along with all of the different parent cells it can reach
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(source[i], k -> new ArrayList<>()).add(uf.getAbsoluteParent(i));
+        }
+        for (int i = 0; i < n; i++) {
+            //if there is no target cell number that means it doesn't exist in source array so it will be difference
+            //also check to see if target cell number has a parent that matches the parent of curr cell i
+            answer += (!map.containsKey(target[i]) || !map.get(target[i]).remove(new Integer(uf.getAbsoluteParent(i)))) ? 1 : 0;
+        }
+        return answer;
+    }
+    private class UnionFind{
+       private int[] parent;
+        
+        private UnionFind(int n){
+            parent = new int[n];
+            for(int i =0; i<n; i++){
+                parent[i] = i;
+            }
+        }
+        private int getAbsoluteParent(int i){
+            if(parent[i] == i){
+                return i;
+            }
+            parent[i] = getAbsoluteParent(parent[i]);
+            return parent[i];
+        }
+        private void unify(int i, int j){
+            int absoluteParentI = getAbsoluteParent(i);
+            int absoluteParentJ = getAbsoluteParent(j);
+            if(absoluteParentI != absoluteParentJ){
+                parent[absoluteParentJ] = absoluteParentI;
+            }
+        }
+    }
+}
